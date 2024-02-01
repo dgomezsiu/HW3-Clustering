88a29eea6e4d8e176907b24165674bdd3def53b6
David Gomez Siu <dgomezsiu@berkeley.edu>
2024-01-31 18:59:43 -0800

    first test

--- test/test_kmeans.py
+++ test/test_kmeans.py
@@ -1,1 +1,12 @@
 # Write your k-means unit tests here
+import numpy as np
+from cluster import KMeans, make_clusters
+import pytest
+
+# test to see if the correct number of clusters is returned, using a stable seed
+def test_clusters():
+    clusters, labels = make_clusters(k = 5, scale = 1, seed = 10)
+    km = KMeans(k = 5)
+    km.fit(clusters)
+    predictions = km.predict(clusters)
+    assert len(np.unique(predictions)) == 5
